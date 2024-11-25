7936d08ec4b3cacf5b3652e5de0840637710f7ed
dswanson <dtswanson@gmail.com>
2024-11-25 11:29:37 +0100

    Starter file added comments

--- get_img/img_move2..py
+++ get_img/img_move2..py
@@ -1,5 +1,3 @@
-"""Rotate, scale and flip an image."""
-
 import pygame
 import math, sys, os
 from pygame.locals import *
@@ -15,7 +13,7 @@ running = True
 
 module = sys.modules['__main__']
 path, name = os.path.split(module.__file__)
-path = os.path.join(path, 'bird.png')
+path = os.path.join(path, 'MIS_Logo.png')
 
 img0 = pygame.image.load(path)
 img0.convert()
